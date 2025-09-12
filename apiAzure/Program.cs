ba3b8fc2e0faa58cc1e31d5b9161106f0389e382
erick <erick.krug@skysoftware.com.br>
2025-09-12 11:35:28 -0300

    Adição de um healthcheck pra que possamos fazer mais testes dentro da azure.

--- apiAzure/Program.cs
+++ apiAzure/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,11 @@ builder.Services.AddScoped<apiAzure.Services.IPersonService, apiAzure.Services.P
 // EF Core DbContext
 builder.Services.AddDbContext<apiAzure.Data.ApiDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
+
+// Health Checks
+builder.Services
+    .AddHealthChecks()  
+    .AddDbContextCheck<apiAzure.Data.ApiDbContext>(name: "postgres-db");
 
 // Repository via EF
 builder.Services.AddScoped<apiAzure.Repositories.IPersonRepository, apiAzure.Repositories.EfPersonRepository>();
@@ -34,8 +40,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// Health check endpoints
+app.MapHealthChecks("/health/live", new HealthCheckOptions
+{
+    Predicate = _ => false // always healthy if app is running
+});
+
+app.MapHealthChecks("/health/ready", new HealthCheckOptions
+{
+    Predicate = _ => true
+});
+
 app.Run();
 
 
 
-/// Commit aleatorio pra ver o CI/CD do github actions em ação, sou foda
+/// Commit aleatorio pra ver o CI/CD do github actions em aï¿½ï¿½o, sou foda
